d69aab3b5cab7db695f4f7306b18cc529d0a5b5a
mansi-patel412 <mansipatel4124@gmail.com>
2026-02-11 11:13:43 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+st.title("EduPro Recommendation System")
+
+# Load data
+learner_profiles = pd.read_csv("learner_profiles.csv")
+courses = pd.read_csv("courses.csv")
+
+# Load model
+kmeans = joblib.load("kmeans_model.pkl")
+
+# Select User
+user_id = st.selectbox("Select User", learner_profiles["UserID"])
+
+user_data = learner_profiles[learner_profiles["UserID"] == user_id]
+
+cluster = user_data["Cluster"].values[0]
+
+st.write("User Cluster:", cluster)
+
+# Dummy recommendation (replace later)
+recommended = courses.sample(5)
+
+st.write("Recommended Courses")
+st.dataframe(recommended)
